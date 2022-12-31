d239bb9ffaf1ca83a742e6666d817f72cca6d745
DavidGMont <davidgarmo@outlook.com>
2022-12-30 20:07:37 -0500

    Cargando la actividad número 1

---
+++ src/T01/Main.java
@@ -0,0 +1,37 @@
+package T01;
+
+public class Main {
+    public static void main(String[] args) {
+        byte number1 = 1;
+        short number2 = 2;
+        int number3 = 3;
+        long number4 = 4;
+        float decimal1 = 4.9f;
+        double decimal2 = 9.99d;
+        double decimal3 = 9.99;
+        char character1 = 'a';
+        boolean verdadero = true;
+        boolean falso = false;
+        String nombre = "Alan";
+        String apellido = "Sastre";
+
+        System.out.println("He aquí los tipos de datos:\n");
+        System.out.println("Numéricos:");
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(decimal1);
+        System.out.println(decimal2);
+        System.out.println(decimal3);
+
+        System.out.println("\nTexto:");
+        System.out.println(character1);
+        System.out.println(nombre);
+        System.out.println(apellido);
+
+        System.out.println("\nBooleano:");
+        System.out.println(verdadero);
+        System.out.println(falso);
+    }
+}
